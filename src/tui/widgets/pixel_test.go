0a0ef454ac86a05ffc847aea7536601a2f4dfe01
Arthur Amos <altriusrs@gmail.com>
2024-12-07 20:07:22 +0000

    added the start of some testing stuff

---
+++ src/tui/widgets/pixel_test.go
@@ -0,0 +1,85 @@
+package widgets
+
+import (
+	"testing"
+)
+
+func TestPixelRenderDefault(t *testing.T) {
+	pixel := NewPixel("=", 0, 0)
+	output := pixel.Render()
+	if output != "=" {
+		t.Errorf("Expected '=', got '%s'", output)
+	} else {
+		t.Log(output)
+	}
+}
+
+func TestPixelRenderBold(t *testing.T) {
+	pixel := NewPixel("=", 0, BOLD)
+	output := pixel.Render()
+	if output != "\x1b[1m=\x1b[0m" {
+		t.Errorf("Expected '\x1b[1m=\x1b[0m', got '%s'", output)
+	} else {
+		t.Log(output)
+	}
+}
+
+func TestPixelRenderItalic(t *testing.T) {
+	pixel := NewPixel("=", 0, ITALIC)
+	output := pixel.Render()
+	if output != "\x1b[3m=\x1b[0m" {
+		t.Errorf("Expected '\x1b[3m=\x1b[0m', got '%s'", output)
+	} else {
+		t.Log(output)
+	}
+}
+
+func TestPixelRenderUnderline(t *testing.T) {
+	pixel := NewPixel("=", 0, UNDERLINE)
+	output := pixel.Render()
+	if output != "\x1b[4m=\x1b[0m" {
+		t.Errorf("Expected '\x1b[4m=\x1b[0m', got '%s'", output)
+	} else {
+		t.Log(output)
+	}
+}
+
+func TestPixelRenderBlink(t *testing.T) {
+	pixel := NewPixel("=", 0, BLINK)
+	output := pixel.Render()
+	if output != "\x1b[5m=\x1b[0m" {
+		t.Errorf("Expected '\x1b[5m=\x1b[0m', got '%s'", output)
+	} else {
+		t.Log(output)
+	}
+}
+
+func TestPixelRenderReverse(t *testing.T) {
+	pixel := NewPixel("=", 0, REVERSE)
+	output := pixel.Render()
+	if output != "\x1b[7m=\x1b[0m" {
+		t.Errorf("Expected '\x1b[7m=\x1b[0m', got '%s'", output)
+	} else {
+		t.Log(output)
+	}
+}
+
+func TestPixelRenderStrikethrough(t *testing.T) {
+	pixel := NewPixel("=", 0, STRIKETHROUGH)
+	output := pixel.Render()
+	if output != "\x1b[9m=\x1b[0m" {
+		t.Errorf("Expected '\x1b[9m=\x1b[0m', got '%s'", output)
+	} else {
+		t.Log(output)
+	}
+}
+
+func TestPixelRenderDim(t *testing.T) {
+	pixel := NewPixel("=", 0, DIM)
+	output := pixel.Render()
+	if output != "\x1b[2m=\x1b[0m" {
+		t.Errorf("Expected '\x1b[2m=\x1b[0m', got '%s'", output)
+	} else {
+		t.Log(output)
+	}
+}
